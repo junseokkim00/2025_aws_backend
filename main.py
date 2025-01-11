7faf0c134cfa8c1f31f1235b5f7d2b5332264213
junseok Kim <junseokkim00@berkeley.edu>
2025-01-11 19:09:14 +0900

    initiate backend file

---
+++ main.py
@@ -0,0 +1,38 @@
+from fastapi import FastAPI
+from assets import PLACESKINS, ACCESSORIES, TALES
+from utils import add_table, add_item, update_item, fetch_all
+
+import json
+import random
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {"Hello": "World"}
+
+@app.get("/get_current_user_info/")
+def get_current_user_info():
+    rows = fetch_all('user_info')
+    user_info = {row[0]: row[1] for row in rows}
+    return user_info
+    
+
+    
+@app.put("/set_current_user_info")
+def set_current_user_info(user_info):
+    update_item(table_name='user_info', new_item=user_info)
+    return "DONE"
+    
+        
+
+@app.get("/get_random_scooter_log/")
+def get_random_scooter_log():
+    with open("./scooter_log.json", "r") as f:
+        data = json.load(f)
+    inst = random.choice(data)
+    return inst
+
+@app.get("/generate_tale")
+def get_tale():
+    return random.choice(TALES)
---
+++ utils.py
@@ -0,0 +1,65 @@
+import sqlite3
+
+
+db_path = "./my_db"
+
+
+def fetch_all(table_name):
+    add_table()
+    connection = sqlite3.connect(db_path)
+    cursor = connection.cursor()
+    rows = cursor.execute(f'SELECT * FROM {table_name}')
+    rows = rows.fetchall()
+    connection.commit()
+    return rows
+    
+
+def add_table():
+    connection = sqlite3.connect(db_path)
+    cursor = connection.cursor()
+    cursor.execute("""CREATE TABLE IF NOT EXISTS inventory (
+id INTEGER NOT NULL,
+name TEXT NOT NULL,
+type TEXT NOT NULL
+);""")
+    cursor.execute("""CREATE TABLE IF NOT EXISTS user_info (
+name TEXT NOT NULL,
+id INT NOT NULL
+);""")
+    rows = cursor.execute(f'SELECT * FROM user_info;')
+    rows = rows.fetchall()
+    if len(rows) != 0:
+        connection.commit()
+    else:
+        cursor.execute("""INSERT INTO user_info (name, id) VALUES ('skin', 0);""")
+        cursor.execute("""INSERT INTO user_info (name, id) VALUES ('scooter', 0);""")
+        cursor.execute("""INSERT INTO user_info (name, id) VALUES ('clean', 0);""")
+        connection.commit()
+
+def update_item(table_name, new_item):
+    """
+    new_item
+        - id
+        - name
+    """
+    add_table()
+    connection = sqlite3.connect(db_path)
+    cursor = connection.cursor(f"UPDATE {table_name} SET id = ? WHERE name = ?", (new_item['id'], new_item['name']))
+    connection.commit()
+
+
+def add_item(table_name, new_item):
+    add_table()
+    connection = sqlite3.connect(db_path)
+    cursor = connection.cursor()
+    column_names = list(new_item.keys())
+    values = tuple(list(new_item.values))
+    columns="("
+    for name in column_names:
+        columns+=f"{name},"
+        columns+=' '
+    columns =columns.strip()
+    columns+=')'
+    cursor.execute(f"INSERT OR IGNORE INTO {table_name} {columns} VALUES (?, ?, ?)", values)
+    connection.commit()
+    
